4caad805f5fd70ba8aaa2282c288344f4b294dfc
Dayvson <dayvsonnlacerda360@gmail.com>
2025-12-12 18:20:29 -0300

    api autenticacao-jwt testada e funcional

--- autenticacao-jwt/autenticacao.js
+++ autenticacao-jwt/autenticacao.js
@@ -45,13 +45,14 @@ app.post('/login', (req, res) => {
 
 
 // Verifica se a token ainda conta
-const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
 
-    if (!token) return res.status(403).json({ message: 'Token não fornecido!' });
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'] || req.headers['Authorization'];
+    
+    if (!token) return res.status(401).json({ message: 'Token não fornecido!' });
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Token inválido!' });
+        if (err) return res.status(401).json({ message: 'Token inválido!' });
         req.user = user;
         next();
     });
